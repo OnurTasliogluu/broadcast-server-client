17dc1ddc06a12ce199329d09f280dd8565b5ca6b
OnurTasliogluu <honor.ston3@gmail.com>
2017-05-01 15:13:15 +0300

    Main ve Server code yer değişikliği
    Main fonksiyonu sadeleştirildi.
    Server ile ilgili olan kısımlar ayrıldıç

--- src/main.c
+++ src/main.c
@@ -1,28 +1,7 @@
 #include "server.h"
 
-int main()
+int32_t main()
 {
-	int     accept_connection[1000];
-    pid_t   accept_child_pid;
-
-    pipe(accept_connection);
-
-    /*
-    	Cocuk process olusturulur.
-    */
-    if((accept_child_pid = fork()) == -1)
-    {
-            DEBUG_PRINT("DEBUG_ERROR : Process olusturulamadi.");
-            return -1;
-    }
-
-    if(accept_child_pid == 0)
-    {
-            // 
-    }
-    else
-    {
-            
-    }
+	server_init();
 	return 0;
 }
--- src/server.c
+++ src/server.c
@@ -1,15 +1,70 @@
 #include "server.h"
 
+#define MAX_CONNECTION 1024
+
+
+/*
+	typedef signed char 	int8_t
+	typedef unsigned char 	uint8_t
+	typedef signed int 	int16_t
+	typedef unsigned int 	uint16_t
+	typedef signed long int 	int32_t
+	typedef unsigned long int 	uint32_t
+	typedef signed long long int 	int64_t
+	typedef unsigned long long int 	uint64_t
+*/
 
 /**
- * @brief 
+ * @brief Client'dan gelen istekleri kabul eder.
  *
- * @param 
+ * @param Socket bağlantısı için gerekli olan "file descripter" degiskenidir.
  *
  * @return 
  *
 */
-int connection_accept()
+void accept_connection()
 {
+	int32_t *fd = socket_fd;
 
+}
+void receive_data()
+{
+	
+}
+void server_init()
+{
+	int32_t	socket_fd[MAX_CONNECTION];
+	pid_t   accept_child_pid;
+	/*
+		Cocuk Process
+	*/
+    pipe(socket_fd);
+
+    /*
+    	Cocuk process olusturulur.
+    */
+    if((accept_child_pid = fork()) == -1)
+    {
+            DEBUG_ERROR("Process olusturulamadi.");
+            return -1;
+    }
+
+    if(accept_child_pid == 0)
+    {
+            /*
+            	Cocuk Process
+            	Bu process sadece client'dan gelen baglantı isteklerini kabul edecektir.
+            */
+    		DEBUG_INFO("Process Olusturuldu - Chiled Process");
+    		void accept_connection((void*)socket_fd);
+    }
+    else
+    {
+            /*
+            	Parent Process
+            	Bu process sadece client'dan gelen verileri alacaktır.
+            */
+    		DEBUG_INFO("Process Olusturuldu - Parent Process");
+    		void receive_data((void*)socket_fd);
+    }
 }
