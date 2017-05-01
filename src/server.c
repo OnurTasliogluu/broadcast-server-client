0d911d6268b49d432c93feb75ac4f9e2febc103c
OnurTasliogluu <honor.ston3@gmail.com>
2017-05-01 20:57:55 +0300

    Ufak hatalar giderildi.
    Bazı noktalı virgül hataları giderildi.

--- src/server.c
+++ src/server.c
@@ -1,7 +1,9 @@
 #include "server.h"
 
-#include <unistd.h>
 #include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <sys/socket.h>
 #include <sys/types.h>
 #include <sys/ipc.h>
 #include <sys/shm.h>
